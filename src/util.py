f39bff6d0724faabade648583e770b7ce33d21d0
James Duncan <jpduncan@berkeley.edu>
2021-09-21 09:46:15 -0700

    Add new notebooks + plotting util

--- src/util.py
+++ src/util.py
@@ -8,6 +8,9 @@ from os import listdir
 from os.path import join as oj
 from cv2 import imread, IMREAD_GRAYSCALE
 
+from matplotlib import gridspec
+import matplotlib.pyplot as plt
+
 def call_and_timing(fn, *args, _fn_name: str=None, _verbose: bool=True, **kwargs):
     start = time.time()
     result = fn(*args, **kwargs)
@@ -51,3 +54,26 @@ def read_vol(path_to_slices, n_slices=None, first_slice=0, verbose=False):
 def save_as_pickle(obj, filepath):
     with open(filepath, 'wb') as f:
         pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+def plot_channels(chs: list, figsize=None, max_quant=.999):
+    R = 1
+    C = len(chs)
+    if C > 3:
+        R = C // 3
+        C = 3
+    figsize = (C+1, R+1) if figsize is None else figsize
+    plt.subplots(figsize=figsize, sharey=True)
+    gs = gridspec.GridSpec(R, C,
+             wspace=0.0, hspace=0.0, 
+             top=1.-0.5/(R+1), bottom=0.5/(R+1), 
+             left=0.5/(C+1), right=1-0.5/(C+1))
+    
+    for r in range(R):
+        for c in range(C):
+            ch_cp = chs[r*3 + c].copy()
+            ch_max = np.quantile(ch_cp, 0.999)
+            ch_cp[ch_cp > ch_max] = ch_max
+            ax = plt.subplot(gs[r,c])
+            ax.imshow(ch_cp, vmin=0, vmax=ch_max)
+
+    plt.show()
